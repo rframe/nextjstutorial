9a1eaca6a1dcb975f90e97978e15c9012d0c2705
Russell Frame <rframe83@gmail.com>
2024-07-07 19:20:25 -0700

    Chapter 9 streaming components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,9 +7,10 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import {LatestInvoicesSkeleton, RevenueChartSkeleton} from '@/app/ui/skeletons';
+import {Suspense} from 'react';
 
 export default async function Page() {
-  const [revenue, latestInvoices, dataCards] = await Promise.all([fetchRevenue(), fetchLatestInvoices(), fetchCardData()]);
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -33,8 +34,12 @@ export default async function Page() {
          />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-         <RevenueChart revenue={revenue}  />
-         <LatestInvoices latestInvoices={latestInvoices} />
+         <Suspense fallback={<RevenueChartSkeleton />}>
+           <RevenueChart />
+         </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
